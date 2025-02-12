9b22320b031eb9fe4a349553e8c839f5c7630db4
AsafeCerceau <asafecerceau.duraes1@gmail.com>
2025-02-12 20:46:41 -0300

    Update Desafio_0040.py

--- Desafios/Desafio_0040.py
+++ Desafios/Desafio_0040.py
@@ -3,16 +3,20 @@
 # Média entre 5.0 e 6.9: RECUPERAÇÃO
 # Média 7.0 ou superior: APROVADO
 
-notaAluno = float(input('Nota do Aluno: '))
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+notaAluno = (nota1 + nota2) / 2
+
+print(f'Sua nota: {notaAluno}')
 
 if 0 < notaAluno < 5.0:
     print('REPROVADO')
-    
+
 elif 5 <= notaAluno <= 6.9:
     print('RECUPERAÇÃO')
-    
+
 elif 10 > notaAluno >= 7.0:
     print('APROVADO')
-    
+
 else:
     print('NOTA INVÁLIDA.')
