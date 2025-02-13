91f2b1c559d1510f6e4891eaab3616e91cde0315
AsafeCerceau <asafecerceau.duraes1@gmail.com>
2025-02-12 22:28:34 -0300

    Update Desafio_0040.py

--- Desafios/Desafio_0040.py
+++ Desafios/Desafio_0040.py
@@ -3,8 +3,23 @@
 # Média entre 5.0 e 6.9: RECUPERAÇÃO
 # Média 7.0 ou superior: APROVADO
 
-nota1 = float(input('Primeira nota: '))
-nota2 = float(input('Segunda nota: '))
+while True:
+    try:
+        nota1 = float(input('Primeira nota: '))
+        if nota1 < 0 or nota1 > 10:
+            print('Por favor, escreva de 0 a 10')
+            continue
+
+        nota2 = float(input('Segunda nota: '))
+        if nota2 < 0 or nota2 > 10:
+            print('Por favor, escreva de 0 a 10')
+            continue
+        
+        break
+        
+    except ValueError:
+        print('Digite um valor válido')
+ 
 notaAluno = (nota1 + nota2) / 2
 
 print(f'Sua nota: {notaAluno}')
