d0a1e76c87cbb231e1fb805ea68bfbf71924e0a8
AsafeCerceau <asafecerceau.duraes1@gmail.com>
2025-02-16 21:49:20 -0300

    Update Desafio_0045.py

--- Desafios/Desafio_0045.py
+++ Desafios/Desafio_0045.py
@@ -6,16 +6,46 @@ while True:
     try:
         print('\nEscolha:')
 
-        pedra = print('1 - Pedra')
-        papel = print('2 - Papel')
-        tesoura = print('3 - Tesoura')
+        print('1 - Pedra')
+        print('2 - Papel')
+        print('3 - Tesoura')
         
         escolha = int(input('Resposta: '))
-        
-        if (escolha == '1') or (escolha == '2') or (escolha == '3'):
-            bot = random.choice[pedra, papel, tesoura]
-            print(bot)
-        
-        break
+         
+        if escolha in [1, 2, 3]: # Isso está aqui dentro do while porque se fizer fora, a escolha vai será feita em strings, mas aqui nós queremos ela como int
+            break
+        else:
+            print('Por favor, escolhe entre 1 e 3.')
+            
     except ValueError:
-        print('Por favor, escolha uma das opções disponíveis.')
+        print('Por favor, escolha uma das opções disponíveis.')       
+        
+
+opcoes = ['Pedra', 'Papel', 'Tesoura']
+bot = random.choice(opcoes) #random.choise retorna um valor // random.choises retorna um lista
+
+#print (bot) #Para verificar se a escolha do bot está dando certo
+
+if escolha ==1:
+    jogador = 'Pedra'
+    
+elif escolha ==2:
+    jogador = 'Papel'
+    
+elif escolha ==3:
+    jogador = 'Tesoura'
+    
+    
+print(f'\nEscolha do Jogador: {jogador}')
+print(f'Escolha do bot {bot}')
+
+if jogador == bot:
+    print('\nEmpate')
+    
+elif (jogador == 'Pedra' and bot == 'Papel') or (jogador == 'Papel' and bot == 'Tesoura') or (jogador == 'Tesoura' and bot == 'Pedra'):
+    print('Você perdeu.')
+        
+else:
+    print('Você venceu.')
+        
+        
